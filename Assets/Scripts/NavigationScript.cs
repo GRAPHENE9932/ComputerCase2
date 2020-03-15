42a77957211f42fa38aaadfa70c6f66cb2bcc64d
GRAPHENE9932 <62153908+GRAPHENE9932@users.noreply.github.com>
2020-03-15 10:31:39 +0200

    Menu completed
    Menu fully completed.
    Added animation of switching.
    Added exit button.

--- Assets/Scripts/NavigationScript.cs
+++ Assets/Scripts/NavigationScript.cs
@@ -3,8 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum MenuState
+{
+    CasesMenu, Inventory, Computer, Shop, Casino, Statistics, Settings
+}
+
 public class NavigationScript : MonoBehaviour
 {
+    /// <summary>
+    ///     Масив відділів меню.
+    /// </summary>
+    public GameObject[] menus;
     /// <summary>
     ///     Основне заднє зобракження меню навігації, на ній маска.
     /// </summary>
@@ -12,16 +21,25 @@ public class NavigationScript : MonoBehaviour
     /// <summary>
     ///     Змінна, що показує, чи відкрите зараз меню.
     /// </summary>
-    private bool menuOpened = true;
+    private bool menuOpened = false;
     /// <summary>
     ///     Змінна, що показує, чи програється зараз анімація закриття або відкриття меню.
     /// </summary>
     private bool menuAnimating;
     /// <summary>
+    ///     Змінна, що показує, чи програється зараз анімація зміни відділу меню.
+    /// </summary>
+    private bool switchAnimating;
+    /// <summary>
+    ///     Відділ меню, на якому зараз знаходиться гравець.
+    /// </summary>
+    private MenuState currentState;
+    /// <summary>
     ///     Подія натиснення на кнопку меню. Запускає корутину анімації, якщо зараз не програється анімація відкриття або закриття.
     /// </summary>
     public void MenuButtonClicked()
     {
+        //Якщо зараз не йде анімація відкриття/закриття меню.
         if (!menuAnimating)
             StartCoroutine(MenuChangeState());
     }
@@ -72,5 +90,77 @@ public class NavigationScript : MonoBehaviour
         mainMenuTransform.sizeDelta = new Vector2(width, mainMenuTransform.sizeDelta.y);
 
         menuAnimating = false;
+    }
+    /// <summary>
+    ///     Подія натиснення на елемент меню.
+    /// </summary>
+    /// <param name="state">
+    ///     Відділ меню, на який треба перейти.
+    /// </param>
+    public void MenuItemClicked(int state)
+    {
+        //Якщо зараз не йде анімація зміни відділу меню.
+        if (!switchAnimating)
+            StartCoroutine(SwitchMenu((MenuState)state));
+    }
+    /// <summary>
+    ///     Перемкнутися між відділами меню.
+    /// </summary>
+    /// <param name="state">
+    ///     Відділ, на який треба перемкнутися.
+    /// </param>
+    private IEnumerator SwitchMenu(MenuState state)
+    {
+        //Якщо відділ, на який треба перейти, не увімкнений зараз...
+        if (state != currentState)
+        {
+            //Зараз програється анімація.
+            switchAnimating = true;
+            //Шлях анімації виходу старого відділу.
+            float fillOut = 0;
+            //Ініціалізація компонентів, над якими буде відбуватися анімація.
+            RectTransform oldTransform = (RectTransform)menus[(int)currentState].transform;
+            CanvasGroup oldGroup = menus[(int)currentState].GetComponent<CanvasGroup>();
+            RectTransform newTransform = (RectTransform)menus[(int)state].transform;
+            CanvasGroup newGroup = menus[(int)state].GetComponent<CanvasGroup>();
+            //Поки fillOut не дійде до 1...
+            while (fillOut < 1)
+            {
+                //Обчислення координати y. Піднесення до куба для плавності.
+                float y = fillOut * fillOut * fillOut * 600F;
+                //Встановлення координати y.
+                oldTransform.anchoredPosition = new Vector2(0, y);
+                //Змінення прозорості рівномірно.
+                oldGroup.alpha = 1F - fillOut;
+                //Збільшення шляху так, щоб через 0.25 сек. він дорівнюівав 1.
+                fillOut += Time.deltaTime * 4;
+                //Очікування кадру.
+                yield return null;
+            }
+            //Вимкнення старого відділу.
+            menus[(int)currentState].SetActive(false);
+            //Увімкнення нового.
+            menus[(int)state].SetActive(true);
+            //Шлях анімації входу нового відділу.
+            float fillIn = 1;
+            //Поки fillIn не впаде до 0...
+            while (fillIn > 0)
+            {
+                //Обчислення координати y. Піднесення до куба для плавності.
+                float y = (1F - fillIn * fillIn * fillIn) * 600F - 600F;
+                //Встановлення координати y.
+                newTransform.anchoredPosition = new Vector2(0, y);
+                //Змінення прозорості рівномірно.
+                newGroup.alpha = 1F - fillIn;
+                //Зменшення шляху так, щоб через 0.25 сек. він дорівнюівав 0.
+                fillIn -= Time.deltaTime * 2;
+                //Очікування кадру.
+                yield return null;
+            }
+            //Фінальна заміна старого відділу на новий
+            currentState = state;
+            //Анімація закінчилась.
+            switchAnimating = false;
+        }
     }
 }
