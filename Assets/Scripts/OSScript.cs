6c8b43176f6ca3f26ffa7b47139ee27597ac8397
GRAPHENE9932 <artemklim50@gmail.com>
2020-05-27 20:50:40 +0300

    InDev 0.6.1
    -Updated OS.
    -Added windows and pages.
    -Started script "OSScript".

---
+++ Assets/Scripts/OSScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum OSPage
+{
+    SystemConfiguration, Recommendations, Mining
+}
+
+public class OSScript : MonoBehaviour
+{
+    OSPage currentPage;
+    public GameObject[] pages;
+
+    public void Navigate(int page)
+    {
+        pages[(int)currentPage].SetActive(false);
+        currentPage = (OSPage)page;
+        pages[page].SetActive(true);
+    }
+
+    public void CloseClicked()
+    {
+        pages[(int)currentPage].SetActive(true);
+    }
+}
