f7e96365f586474daa1bfa5d34848127f1452993
GRAPHENE9932 <artemklim50@gmail.com>
2020-07-06 18:46:44 +0300

    InDev 14.1
    -Quit window created.

--- Assets/Scripts/ExitScript.cs
+++ Assets/Scripts/ExitScript.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class ExitScript : MonoBehaviour
 {
+    public Animation windowAnim;
     /// <summary>
     ///     Вихід.
     /// </summary>
-    public void Exit()
+    public void Exit_Clicked()
     {
-        Application.Quit();
+        windowAnim.gameObject.SetActive(true);
+        windowAnim.Play("Open quit window");
+    }
+
+    public void Yes() => Application.Quit();
+
+    public void No() => StartCoroutine(NoCorutine());
+
+    private IEnumerator NoCorutine()
+    {
+        windowAnim.Play("Close quit window");
+        yield return new WaitForSeconds(windowAnim.GetClip("Close quit window").length);
+        windowAnim.gameObject.SetActive(false);
     }
 }
